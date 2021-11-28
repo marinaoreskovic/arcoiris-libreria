bdb0a3ad2ce033f296a459465fec754e30abda24
Marina Oreskovic <44814229+marinaoreskovic@users.noreply.github.com>
2021-11-28 16:38:52 -0300

    Creacion de la app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Arcoíris</h1>
+        <p>Libreria y Pepeleria</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
