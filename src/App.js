b2c02058fa2402cccd655b4a8e7062c55fda05e9
Marina <marinacelesteoreskovic@gmail.com>
2021-11-30 05:31:33 -0300

    Agregue ItemCount

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import { NavBar } from "./components/NavBar/NavBar";
+import { ItemCount } from "./components/ItemCount/ItemCount";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <ItemListContainer greeting="Bienvenido a Libreria Arcoíris!" />
+      <ItemCount />
     </div>
   );
 }
